4934cd9484ac04e04a4dfc63094bb75514a323a6
stijn <stijn_hanss13@live.com>
2024-01-08 18:11:17 +0100

    add interface/props and api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-const App = () => {
-  return <div>App</div>;
+import axios from "axios";
+import { FC, useEffect } from "react";
+
+interface AppProps {
+  title: string;
+}
+
+const App: FC<AppProps> = ({ title }) => {
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://randomuser.me/api/?results=10"
+        );
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return <h1>{title}</h1>;
 };
 
 export default App;
