3f480f2dc4c7bbae7f303df8a624e5b5e39881e8
stijn <stijn_hanss13@live.com>
2024-01-08 18:31:08 +0100

    define list from api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import axios from "axios";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface AppProps {
   title: string;
 }
 
+export interface Name {
+  first: string;
+  last: string;
+}
+
+export interface Login {
+  uuid: string;
+}
+
+export interface Users {
+  name: Name;
+  login: Login;
+  email: string;
+}
+
 const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<Users[]>([]);
+
   useEffect(() => {
     const getUsers = async () => {
       try {
@@ -13,6 +30,7 @@ const App: FC<AppProps> = ({ title }) => {
           "https://randomuser.me/api/?results=10"
         );
         console.log(data);
+        setUsers(data.results);
       } catch (error) {
         console.log(error);
       }
@@ -20,7 +38,24 @@ const App: FC<AppProps> = ({ title }) => {
     getUsers();
   }, []);
 
-  return <h1>{title}</h1>;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {users.map(({ login, name, email }) => {
+          return (
+            <li key={login.uuid}>
+              <div>
+                Name: {name.first} {name.last}
+              </div>
+              <div>Email: {email}</div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
